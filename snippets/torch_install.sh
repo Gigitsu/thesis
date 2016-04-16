145c6650811b45ab131d3b3e443e16c5197c6a72
Gigitsu <gigitsu.23@gmail.com>
2016-04-16 14:18:29 +0200

    Added conclusions

--- snippets/torch_install.sh
+++ snippets/torch_install.sh
@@ -1,6 +1,6 @@
 $ curl -s \
-    https://raw.githubusercontent.com/torch/ezinstall/master/install-deps | \
-    bash
+    https://raw.githubusercontent.com/torch/ezinstall/master/install-deps \
+    | bash
 $ git clone https://github.com/torch/distro.git ~/torch --recursive
 $ cd ~/torch;
 $ ./install.sh      # and enter "yes" at the end to modify your bashrc
